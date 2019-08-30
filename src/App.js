7e416b07ca97d98720b7867e10fe65c2fa19149a
Shawn <sppang1@yahoo.com>
2019-08-29 22:30:48 -0400

    Begin creation of portis survey app

--- src/App.js
+++ src/App.js
@@ -2,23 +2,66 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { Button } from 'react-bootstrap'
+import { Nav, Navbar, NavDropdown, Form, FormControl, Container } from 'react-bootstrap'
+import { ListGroup, ListGroupItem } from 'react-bootstrap'
+
+import Portis from '@portis/web3';
+import Web3 from 'web3';
+
+const portis = new Portis('5df91c2d-4e4b-4c86-a1bc-4a85b4f6ae33', 'mainnet');
+const web3 = new Web3(portis.provider);
+
+/*portisAccount = () => {
+  web3.eth.getAccounts().then(accounts => {
+    document.getElementById("portis").innerHTML = `<p>Wallet Address: ${
+      accounts[0]
+      }</p>`;
+  });
+}*/
+function retrievePortisAddress() {
+  web3.eth.getAccounts().then(accounts => {
+    document.getElementById("portis").innerHTML = `<p>Wallet Address: ${
+      accounts[0]
+      }</p>`;
+  });
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">Portis Surveys</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Navbar>
+
+      <ListGroup>
+        <ListGroup.Item action onClick={retrievePortisAddress}>Do you like Cats?</ListGroup.Item>
+        <ListGroup.Item>Do you like Dogs?</ListGroup.Item>
+        <ListGroup.Item>Do you like animals?</ListGroup.Item>
+        <ListGroup.Item>Do you like portis?</ListGroup.Item>
+        <ListGroup.Item>Are you the Video Game Boy?</ListGroup.Item>
+      </ListGroup>
+      <div id="portis"></div>
+      <Button variant="primary">Create New Survey</Button>
     </div>
   );
 }
